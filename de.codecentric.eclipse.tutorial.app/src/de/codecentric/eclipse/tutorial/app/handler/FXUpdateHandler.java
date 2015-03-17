c64ce48d6fac13d323df05e0bec540e00dcb8d05
Dirk Fauth <dirk.fauth@googlemail.com>
2015-03-17 14:13:38 +0100

    Updated usage of e(fx)clipse services and extended update mechanism to ask whether an update should be performed or not

--- de.codecentric.eclipse.tutorial.app/src/de/codecentric/eclipse/tutorial/app/handler/FXUpdateHandler.java
+++ de.codecentric.eclipse.tutorial.app/src/de/codecentric/eclipse/tutorial/app/handler/FXUpdateHandler.java
@@ -2,39 +2,67 @@ package de.codecentric.eclipse.tutorial.app.handler;
 
 import java.util.Optional;
 
-
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
-
+import javafx.scene.control.ButtonType;
 
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.fx.core.ReturnValue.State;
 import org.eclipse.fx.core.update.UpdateService;
+import org.eclipse.fx.ui.services.restart.RestartService;
 import org.eclipse.fx.ui.services.sync.UISynchronize;
 
 public class FXUpdateHandler {
 
 	@Execute
-	public void execute(UpdateService service, IWorkbench workbench, UISynchronize sync) {
-		service.checkUpdate((ucd) -> {
-			
-			if (State.OK.equals(ucd.getState())) {
-				service.update(ucd, (data) -> {
-					
-					sync.syncExec(() -> {
-						Alert alert = new Alert(AlertType.CONFIRMATION);
-						alert.setTitle("Updates installed, restart?");
-						alert.setContentText("Updates have been installed successfully, do you want to restart?");
+	public void execute(UpdateService updateService, RestartService restartService, UISynchronize sync) {
+		updateService.checkUpdate((ucd) -> {
+			if (ucd.nothingToUpdate()) {
+				showMessage(sync, "Nothing to update");
+			}
+			else {
+				if (State.OK.equals(ucd.getState())) {
+					if (showConfirmation(
+							sync, 
+							"Updates available", 
+							"There are updates available. Do you want to install them now?")) {
 						
-						Optional<ButtonType> result = alert.showAndWait();
-						if (result.get() == ButtonType.OK){
-							workbench.restart();
-						}
-					});
-				});
+						updateService.update(ucd, (data) -> {
+							
+							if (showConfirmation(
+									sync, 
+									"Updates installed, restart?", 
+									"Updates have been installed successfully, do you want to restart?")) {
+								
+								restartService.restart(true);
+							}
+						});
+					}
+				}
 			}
 		});
 	}
+	
+    private void showMessage(UISynchronize sync, final String message) {
+        // as the provision needs to be executed in a background thread
+        // we need to ensure that the message dialog is executed in 
+        // the UI thread
+        sync.syncExec(() -> {
+        	Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Information");
+            alert.setContentText(message);
+
+            alert.showAndWait();
+        });
+    }
+
+    private boolean showConfirmation(UISynchronize sync, final String title, final String message) {
+    	return sync.syncExec(() -> {
+    		Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle(title);
+            alert.setContentText(message);
+            Optional<ButtonType> result = alert.showAndWait();
+            return (result.get() == ButtonType.OK);
+    	}, false);
+    }
 }
